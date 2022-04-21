c67d2ced99a09d358ccee6753296270ef36cd948
David Eglin <hello@djegl.in>
2022-04-21 11:43:13 +0100

    update studio branding

---
+++ studio/parts/logo/brandLogo.js
@@ -0,0 +1,29 @@
+import React from 'react'
+
+import styles from './brandLogo.css'
+
+const frntLogo = () => (
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 149 182" fill="none" className={styles.logo}>
+    <path d="M98.8854 166V107.813H79V95H134V107.813H114.018V166H98.8854Z" fill="currentcolor" />
+    <path
+      d="M16 166V95H33.5728L59.5 140.953V95H74V166H59.0199L33.3808 120.433H30.5V166H16Z"
+      fill="currentcolor"
+    />
+    <path
+      d="M16 87V16H66V28.8128H30.914V45.5753H61.6979V57.9064H30.914V87H16Z"
+      fill="currentcolor"
+    />
+    <path
+      d="M77 87V16H121.823L132.499 26.6934V51.1628L123.554 60.0258L135 87H119.129L108.934 61.8562H92.005V87H77ZM92.005 49.7178H113.935L117.783 45.9606L117.687 31.8955L113.935 28.1384H92.005V49.7178Z"
+      fill="currentcolor"
+    />
+    <path
+      fill-rule="evenodd"
+      clip-rule="evenodd"
+      d="M8 8H141V174H8V8ZM149 174H141V182H8H0V174V8V0H8H141H149V8V174Z"
+      fill="currentcolor"
+    />
+  </svg>
+)
+
+export default frntLogo
